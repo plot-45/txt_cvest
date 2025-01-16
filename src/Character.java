8dfbcf0d7abbbc66ac4293fce010a27e4f4833a8
plot-45 <kzhurkin2010@gmail.com>
2025-01-16 17:28:25 +0700

    z
    z-z-z

---
+++ src/Character.java
@@ -0,0 +1,13 @@
+public class Character {
+    public int K;
+    public int A;
+    public int R;
+    public String name;
+
+    public Character(String name) {
+        K = 1;
+        A = 100;
+        R = 50;
+        this.name = name;
+    }
+}
